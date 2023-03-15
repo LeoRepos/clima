92f4cfe8576bc42a47d21bb4a55747748f75fecb
Leonardo dos Santos Dias <dias.leonardodossantos@gmail.com>
2023-03-14 22:34:01 -0300

    feat/ Concluindo o projeto

---
+++ script.js
@@ -0,0 +1,53 @@
+const API_KEY = 'c0073a9f9e8ca0ef21da8db2247c66da'; 
+
+document.querySelector('.busca').addEventListener('submit', async (e) => {
+    //* evita que o formulário seja enviado
+    e.preventDefault();
+    
+    let input = document.querySelector('#searchInput').value;
+    
+    if (input !== '') {
+        clearInfo();
+        showWarning('Carregando...');
+
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=${API_KEY}&units=metric&lang=pt_br`;
+        const results = await fetch(url);
+        const json = await results.json();
+        
+        if (json.cod === 200) {
+            showInfo({
+                name:json.name,
+                country:json.sys.country,
+                temp: json.main.temp,
+                tempIcon: json.weather[0].icon,
+                windSpeed: json.wind.speed,
+                windAngle: json.wind.deg
+            });
+        } else {
+            clearInfo();
+            showWarning('Não encontramos esta localização..!');
+        }
+    } else {
+        clearInfo();
+    };
+
+});
+
+function showInfo(json){
+    showWarning('');
+    document.querySelector('.resultado').style.display = 'block';
+    document.querySelector('.titulo').innerHTML        = `${json.name}, ${json.country}`;
+    document.querySelector('.tempInfo').innerHTML      = `${json.temp} <sup>ºC</sup>`;
+    document.querySelector('.ventoInfo').innerHTML     = `${json.windSpeed} <span>km/h</span> `;    
+    document.querySelector('.temp img').setAttribute('src',`http://openweathermap.org/img/wn/${json.tempIcon}@2x.png`);    
+    document.querySelector('.ventoPonto').style.transform = `rotate(${json.windAngle-90}deg)`;
+}
+
+function clearInfo() {
+    showWarning('');
+    document.querySelector('.resultado').style.display = 'none';
+}
+
+function showWarning(msg) {
+    document.querySelector('.aviso').innerHTML = msg;
+}
